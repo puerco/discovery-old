843fcf7ef0d76df933a92a9322ae2ff1b1d96ec9
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-11-28 16:52:26 -0600

    Add documentation to VexProbe
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/discovery/package_probe.go
+++ pkg/discovery/package_probe.go
@@ -10,6 +10,13 @@ import (
 	"github.com/openvex/discovery/pkg/discovery/options"
 )
 
+// VexProbe abstracts a backend driver. The main goal of a probe is to
+// capture the logic to find vex documents from software identifiers and
+// other references.
+//
+// The initial version of the VexProbe interface exposes a FindDocumentsFromPurl()
+// method that takes an options struct and a Package URL. The VEX probe captures
+// the logic to find VEX data for the purl type.
 type VexProbe interface {
 	FindDocumentsFromPurl(options.Options, purl.PackageURL) ([]*vex.VEX, error)
 	SetOptions(options.Options)
